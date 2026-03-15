f8d92471c523c9b674f58aa646bea20db0930fdc
undefined06855 <currentcashew57@gmail.com>
2026-03-15 09:32:00 +0000

    add popup for nonexistent texture

--- src/popup.cpp
+++ src/popup.cpp
@@ -4,6 +4,13 @@
 $on_game(Loaded) {
     FLAlertLayer* popup = nullptr;
 
+    std::error_code ec;
+
+    auto path = geode::utils::string::pathToString(geode::Mod::get()->getSettingValue<std::filesystem::path>("image-path"));
+    auto texture = cocos2d::CCTextureCache::get()->addImage(path.c_str(), true);
+
+    // i think these are the most arbitrary ive made popup text colours before
+
     if (g_transitionShaderFailedToLoad) {
         popup = FLAlertLayer::create(
             "Custom Transitions",
@@ -18,6 +25,18 @@ $on_game(Loaded) {
             "Please go into the <cy>mod settings</c> and select an <co>image</c> "
             "for the mod to use for the transition!\n\n"
             "Greyscale images are <cl>recommended</c>.",
+            "Open mod settings", nullptr,
+            [](auto, auto) {
+                geode::openSettingsPopup(geode::Mod::get(), false);
+            },
+            false
+        );
+    } else if (!texture) {
+        popup = geode::createQuickPopup(
+            "Custom Transitions",
+            "The <co>custom image</c> used for the transitions <cr>does not exist</c> (or is invalid)!\n"
+            "Please set a <cy>new image</c> in the <cl>mod settings</c>.\n\n"
+            "(only <cc>PNG</c>, <ca>JPG</c> and <cp>TIFF</c> files are allowed)",
             "Open mod settings", nullptr,
             [](auto, auto) {
                 geode::openSettingsPopup(geode::Mod::get(), false);

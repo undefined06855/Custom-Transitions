fae5769463758b79cecf434a79f987abf935b38c
undefined06855 <currentcashew57@gmail.com>
2026-03-15 13:46:16 +0000

    fix some stuff to remove it

--- src/copy-on-startup.cpp
+++ src/copy-on-startup.cpp
@@ -1,7 +1,11 @@
 $on_mod(Loaded) {
     std::error_code ec;
-    std::filesystem::copy(geode::Mod::get()->getResourcesDir(), geode::Mod::get()->getConfigDir(), ec);
+    std::filesystem::remove_all(geode::Mod::get()->getConfigDir(), ec);
+    if (ec) {
+        geode::log::warn("Could not remove config dir: {} ({})", ec.message(), ec.value());
+    }
 
+    std::filesystem::copy(geode::Mod::get()->getResourcesDir(), geode::Mod::get()->getConfigDir(), ec);
     if (ec) {
         geode::log::warn("Could not copy transition examples: {} ({})", ec.message(), ec.value());
     }
